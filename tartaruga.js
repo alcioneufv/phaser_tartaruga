47868a05023d80a7bacdf13584c076df98a31231
alcioneufv <95593589+alcioneufv@users.noreply.github.com>
2022-01-24 09:33:41 -0300

    Update tartaruga.js

--- tartaruga.js
+++ tartaruga.js
@@ -31,14 +31,18 @@ function create() {
     // Cria a tartaruga
     //*******************
 
-    this.anims.create({
-            key: 'idle',
-            frames: this.anims.generateFrameNumbers('tarta', { frames: [ 0, 1 ] }),
-            frameRate: 8,
-            repeat: -1
-        });
+    this.anims.create({ key: 'idle', frames: this.anims.generateFrameNumbers('tarta', { frames: [ 0, 1 ] }),
+            frameRate: 8, repeat: -1 });
+    this.anims.create({ key: 'left', frames: this.anims.generateFrameNumbers('tarta', { frames: [ 6, 7 ] }),
+            frameRate: 5, repeat: -1 });
+    this.anims.create({ key: 'right', frames: this.anims.generateFrameNumbers('tarta', { frames: [ 4, 5 ] }),
+            frameRate: 5, repeat: -1 });
+    this.anims.create({ key: 'up', frames: this.anims.generateFrameNumbers('tarta', { frames: [ 0, 1 ] }),
+            frameRate: 5, repeat: -1 });
+    this.anims.create({ key: 'down', frames: this.anims.generateFrameNumbers('tarta', { frames: [ 2, 3 ] }),
+            frameRate: 5, repeat: -1 });
     
-    this.add.sprite(0, 0, 'tarta')
+    this.add.sprite(300, 300, 'tarta')
             .play('idle');
     /*
     player = this.add.sprite(50, 50, 'tarta', 1);
