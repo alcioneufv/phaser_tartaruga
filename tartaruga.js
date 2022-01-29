918141d680e47fe113f603e5a0f9201696ea15a6
alcioneufv <95593589+alcioneufv@users.noreply.github.com>
2022-01-29 20:06:57 -0300

    Update tartaruga.js

--- tartaruga.js
+++ tartaruga.js
@@ -43,7 +43,7 @@ function create() {
 
     this.anims.create({ key: 'idle', frames: this.anims.generateFrameNumbers('tarta', { frames: [ 0, 1 ] }), frameRate: 5, repeat: -1 });
     this.anims.create({ key: 'left', frames: this.anims.generateFrameNumbers('tarta', { frames: [ 6, 7 ] }), frameRate: 10, repeat: -1 });
-    this.anims.create({ key: 'right', frames: this.anims.generateFrameNumbers('tarta', { frames: [ 4, 5 ] }), frameRate: 10, repeat: -1 });
+    this.anims.create({ key: 'right', frames: this.anims.generateFrameNumbers('tarta', { frames: [ 4, 5 ] }), frameRate: 30, repeat: -1 });
     this.anims.create({ key: 'up', frames: this.anims.generateFrameNumbers('tarta', { frames: [ 0, 1 ] }), frameRate: 10, repeat: -1 });
     this.anims.create({ key: 'down', frames: this.anims.generateFrameNumbers('tarta', { frames: [ 2, 3 ] }), frameRate: 10, repeat: -1 });
     
